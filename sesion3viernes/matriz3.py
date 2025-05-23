4c75b755f539dd8e34f2e87ebe2a5f147638774d
andyrueda439 <ajrueda@uamv.edu.ni>
2025-05-23 08:15:19 -0500

    Nombres y numeros

--- sesion3viernes/matriz3.py
+++ sesion3viernes/matriz3.py
@@ -1,7 +1,24 @@
-matriz = [["axel", "oscar", "joshua"], ["alfredo", "donald", "andy"], ["kevin", "cristopher", "josue"]]
+matriz = [["Axel", "Oscar", "Joshua"], 
+          ["Alfredo", "Donald", "Andy"],
+            ["Kevin", "Cristopher", "Josue"]]
+
+cant_letras = []
+
 print("-"*29)
 for fila in matriz:
     for columna in fila:
-        print(f"|{columna:>6}", end = " ")    
+        print(f"|{columna}", end = " ")    
     print("|")
     print("-"*29)
+
+for fila in matriz:
+    new_row=[]
+    for columna in fila:
+        new_row.append(len(columna))
+    cant_letras.append(new_row)
+
+for fila in cant_letras:
+    for columna in fila:
+        print(f"|{columna}", end = " ")
+    print("|")
+    print("-"*17)
