5146dcdbe8e6b20fe0d415d7d4bd70f67c2d5170
andyrueda439 <ajrueda@uamv.edu.ni>
2025-05-23 08:04:24 -0500

    Decimal

--- sesion3viernes/matriz2.py
+++ sesion3viernes/matriz2.py
@@ -1,7 +1,7 @@
 matriz = [[10.5, 2.5], [300.03, 4.71]]
-print("-"*17)
+print("-"*16)
 for fila in matriz:
     for columna in fila:
         print(f"|{columna:>6.1f}", end = " ")    
     print("|")
-    print("-"*17)
+    print("-"*16)
