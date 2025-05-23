a3736b6176a7928ca873ae3d5a1e2a1f6a7af9d4
andyrueda439 <ajrueda@uamv.edu.ni>
2025-05-23 07:23:33 -0500

    Se actualizo el buscador de palabras

--- sesion2/ejercicio4.py
+++ sesion2/ejercicio4.py
@@ -1,8 +1,10 @@
 # 4. Buscar un elemento
 def buscar_palabra():
     lista = ["manzana", "banana", "uva", "pera", "sandía"]
-    palabra = input("Ingresa una palabra: ")
+    palabra = input("Ingresa una palabra: ").strip()
     if palabra in lista:
         print("La palabra está en la lista.")
     else:
         print("La palabra NO está en la lista.")
+
+buscar_palabra()
